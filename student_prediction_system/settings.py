a4eaeb15cae81e9f97cbef14b7a8398a320ced02
KAMALI KASTHURI K <your-email@example.com>
2026-02-19 15:43:18 +0530

    Fixed template context processors

--- student_prediction_system/settings.py
+++ student_prediction_system/settings.py
@@ -54,8 +54,16 @@ ROOT_URLCONF = 'student_prediction_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],   # must be like this
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
 ]
 
