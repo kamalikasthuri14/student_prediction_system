924172a9a0637a5f0415f64ac51e7cdf02152b0e
KAMALI KASTHURI K <your-email@example.com>
2026-02-19 15:10:15 +0530

    Added login template

--- student_prediction_system/settings.py
+++ student_prediction_system/settings.py
@@ -54,16 +54,8 @@ ROOT_URLCONF = 'student_prediction_system.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],   # must be like this
         'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
     },
 ]
 
